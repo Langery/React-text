fd8551180fb78be3926aac20b3e8cbe817594ad8
Langery <1245066197@qq.com>
2019-01-01 15:47:11 +0800

    add files

---
+++ Next/pages/index.js
@@ -0,0 +1,13 @@
+// This is the Link API
+// import Link from 'next/link'
+// import Header from '../components/Header'
+
+import Layout from '../components/MyLayout'
+
+const Index = () => (
+  <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
+)
+
+export default Index
