38b3b1dd2edba238fbe2feb3002b6b4679cafb7d
Langery <1245066197@qq.com>
2019-04-27 13:40:34 +0800

    sidebar

--- Ant-Design/src/view/mainView/homeIndex.js
+++ Ant-Design/src/view/mainView/homeIndex.js
@@ -1,26 +1,14 @@
 import React, { Component } from "react";
-import { Row, Col } from 'antd';
 import './homeIndex.css'
 import 'antd/dist/antd.css';
+import SiderDemo from '../components/sidebar/sidebar'
 
 class IndexHome extends Component {
   render () {
     return (
       <div>
-        <Row>
-          <Col span={12} className="row1">col-12</Col>
-          <Col span={12}>col-12</Col>
-        </Row>
-        
-        <Row>
-          <Col span={8}>col-8</Col>
-          <Col span={8} className="row2">col-8</Col>
-          <Col span={8}>col-8</Col>
-        </Row>
-        <Row>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+        {/* main frame */}
+        <SiderDemo></SiderDemo>
       </div>
     );
   }
